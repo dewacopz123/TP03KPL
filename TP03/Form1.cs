0ff5b6ffb21ded81104e7f383f77cd4faa82976a
MUHAMMAD ZAKY AL-FARIZY <zakyfaryz@student.telkomuniversity.ac.id>
2025-03-03 21:28:23 +0700

    Add project files.

---
+++ TP03/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += new EventHandler(button1_Click); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputNama = textBox1.Text.Trim(); 
+
+            if (!string.IsNullOrEmpty(inputNama))
+            {
+                label1.Text = "Halo, " + inputNama; 
+            }
+            else
+            {
+                label1.Text = "Silakan masukkan nama!";
+            }
+        }
+    }
+}
